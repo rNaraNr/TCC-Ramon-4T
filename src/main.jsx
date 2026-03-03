539fe73017b2a899aae208ec7217090394b85fb7
rNaraN <naracarvalho@protonmail.com>
2026-03-03 15:38:50 -0300

    Definidas todas as cores do projeto no index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <p>aaaa</p>
+    <>
+        <h1 className='text-9xl'>Fira Sans</h1>
+        <h1 className='cor-discreto text-9xl'>Fira Sans</h1>
+        <h1 className='cor-destaque text-9xl'>Fira Sans</h1>
+        <h1 className='cor-destaque cor-discreto text-9xl'>Fira Sans</h1>
+    </>
 )
