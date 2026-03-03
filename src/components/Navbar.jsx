f7c85637c4e7b75b8a7128d11e56e41d0df95b51
rNaraN <naracarvalho@protonmail.com>
2026-03-03 17:49:56 -0300

    testes de navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import '../index.css'
+
+function Navbar() {
+    return (
+        <nav class='border-t-2 border-discreto
+        fixed bottom-0 flex justify-between px-3 w-[100vw]'>
+            <div>Logo</div>
+
+            <div>
+                a
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import Nav from './components/Navbar.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
+        <Nav />
         <h1 className='text-9xl'>Fira Sans</h1>
-        <h1 className='cor-discreto text-9xl'>Fira Sans</h1>
-        <h1 className='cor-destaque text-9xl'>Fira Sans</h1>
-        <h1 className='cor-destaque cor-discreto text-9xl'>Fira Sans</h1>
+        <h1 className='text-discreto text-9xl'>Fira Sans</h1>
+        <h1 className='bg-destaque text-9xl'>Fira Sans</h1>
+        <h1 className='bg-destaque text-discreto text-9xl'>Fira Sans</h1>
     </>
 )
