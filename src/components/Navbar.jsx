5b43f21a3c54bc3bb8fded704a616d437fcb14be
rNaraN <naracarvalho@protonmail.com>
2026-03-08 13:05:08 -0300

    Push sem motivo nenhum pra eu poder trabalhar no meu pc (fiz umas mudanças na navbar e eras isso)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import '../index.css'
 
 function Navbar() {
     return (
-        <nav class='border-t-2 border-discreto
-        fixed bottom-0 flex justify-between px-3 w-screen'>
+        <nav className='border-t-2 border-discreto fixed bottom-0 flex justify-between px-3 w-screen'>
             <div>Logo</div>
 
             <div>
-                <a>aaaa</a>
+                <a className='nav w-12 h-12'>
+                    <svg>
+                        <path  d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+                    </svg>
+                </a>
             </div>
-
         </nav>
     )
 }
